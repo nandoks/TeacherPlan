9607c8f152424936a3ebe1f03ad6ee61f50e8825
nandoksman <fernandooksman@gmail.com>
2021-07-06 12:00:07 -0300

    added profiles to project settings (dev, prod)

---
+++ TeacherPlan/settings/dev.py
@@ -0,0 +1,7 @@
+from .base import *
+
+SECRET_KEY = 'django-insecure-13y*c09bk*4vja4(p*i9kkufghkb%4$w%gib3gprjvg4743xks'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ TeacherPlan/settings/prod.py
@@ -0,0 +1,9 @@
+from .base import *
+import os
+
+
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['localhost', ]
